f82312b855cea54bb3a523339ae99ad5df712780
Shubham Tiwari <shubham.tiwari@techchefz.com>
2024-08-13 21:20:09 +0530

    feat: remove from cart on product page

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React from "react";
 
 interface ProductCardProps {
   imageUrl: string;
   name: string;
   price: number;
   onAddToCart: () => void;
+  onRemoveFromCart: () => void;
+  isAddedToCart: Boolean;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ imageUrl, name, price, onAddToCart }) => {
+const ProductCard: React.FC<ProductCardProps> = ({
+  imageUrl,
+  name,
+  price,
+  onAddToCart,
+  isAddedToCart,
+  onRemoveFromCart,
+}) => {
   return (
     <div className="pt-4 overflow-hidden bg-white rounded-b-lg shadow-sm hover:shadow-product hover:cursor-pointer">
       <img src={imageUrl} alt={name} className="object-cover w-full h-48" />
       <div className="p-4">
         <h2 className="mb-2 text-lg font-semibold">{name}</h2>
         <p className="mb-4 text-gray-600">${price.toFixed(2)}</p>
-        <button 
-          onClick={onAddToCart}
-          className="w-full px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-        >
-          Add to Cart
-        </button>
+        {!isAddedToCart ? (
+          <button
+            onClick={onAddToCart}
+            className="w-full px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+          >
+            Add to Cart
+          </button>
+        ) : (
+          <button
+            onClick={onRemoveFromCart}
+            className="w-full px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600"
+          >
+            Remove from cart
+          </button>
+        )}
       </div>
     </div>
   );
--- src/Pages/ProductListing.tsx
+++ src/Pages/ProductListing.tsx
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { RootState } from '../Store/index';
 import { setProducts } from '../Store/slices/productSlice';
-import { addToCart } from '../Store/slices/cartSlice';
+import { addToCart, removeFromCart } from '../Store/slices/cartSlice';
 
 interface Product {
     id: number;
@@ -20,10 +20,17 @@ interface Product {
 const ProductListing: React.FC = () => {
     const dispatch = useDispatch()
     const products = useSelector((state:RootState) => state.product.products)
+    const cartItems = useSelector((state:RootState) => state.cart.items)
+    
 
     const handleAddToCart = (product:Product) => {
       dispatch(addToCart(product));
     };
+
+    const handleRemoveFromCart = (productID:number) => {
+      dispatch(removeFromCart(productID));
+    };
+
     
 
 
@@ -35,11 +42,13 @@ const ProductListing: React.FC = () => {
     <div className="container grid grid-cols-1 gap-4 px-8 py-4 mx-auto md:grid-cols-3 md:px-16 lg:px-16 sm:grid-cols-2 lg:grid-cols-4">
     {products.map((product) => (
       <ProductCard
-        key={product.id}
-        imageUrl={product.imageUrl}
-        name={product.name}
-        price={product.price}
-        onAddToCart={() => handleAddToCart(product)}
+      key={product.id}
+      imageUrl={product.imageUrl}
+      name={product.name}
+      price={product.price}
+      onAddToCart={() => handleAddToCart(product)}
+      onRemoveFromCart={()=>handleRemoveFromCart(product.id)}
+      isAddedToCart={cartItems.find(item => item.id === product.id)? true : false}
       />
     ))}
   </div>
