567a3689d902b930e2619bf05016f812f6f92e93
Shubham Tiwari <shubham.tiwari@techchefz.com>
2024-08-13 09:55:54 +0530

    feat : added routing for cart and products page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import ProductListing from './Pages/ProductListing'
+import Cart from './Pages/Cart'
 
 function App() {
 
   return (
     <>
+    <Router>
      <Navbar/>
+     <Routes>
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
     </>
   )
 }
