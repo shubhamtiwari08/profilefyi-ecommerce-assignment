5222a32fa41a52b6b142e27e54df776fcfe20098
Shubham Tiwari <shubham.tiwari@techchefz.com>
2024-08-19 00:21:43 +0530

    fix : text alignment

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <div className='flex flex-col items-center justify-between w-full gap-1 px-16 py-8 mx-auto mt-6 text-sm text-gray-500 border-t border-gray-300 md:flex-row '>
      <span className='text-center'>Policies: Return Policy | Terms of Use | Security | Privacy | Infringement</span>
       <span>© 2007-2024 Shipart</span>
-      <span>Need Help ? Visit the help center.</span>
+      <span className='text-center'>Need Help ? Visit the help center.</span>
     </div>
   );
 };
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
       <main >
         <div className='bg-white w-[80%] max-h-max border border-gray-200 mx-auto mt-10 flex justify-center flex-col items-center p-8'>
           <h1 className='p-1 text-xl text-blue-500 rounded-sm'>Welcome to</h1>
-          <span className='px-4 py-1 text-3xl font-semibold text-white bg-yellow-300 rounded-sm'>Artist's Home</span>
+          <span className='px-4 py-1 text-3xl font-semibold text-center text-white bg-yellow-300 rounded-sm'>Artist's Home</span>
            <strong className='text-blue-500 text-8xl font-lobster'>ShipArt</strong>
            <Link to={'/products'} >
          <button className="px-4 py-2 mt-10 font-semibold text-white uppercase transition-all duration-700 ease-in-out transform scale-100 bg-orange-500 opacity-100 hover:bg-orange-600">Browse Products</button>
